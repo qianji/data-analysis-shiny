aff6a5d1cd3b1d28b5e2f30d85fba6d099bb6fd5
qianji <qianji.zheng@gmail.com>
2014-09-02 19:47:05 -0500

    Different tabs for different sectors

--- server.R
+++ server.R
@@ -11,19 +11,13 @@ library(ggplot2)
 shinyServer(function(input, output) {
   
   # Filter data based on selections
-  output$table <- renderDataTable({
+  output$industry <- renderDataTable({
     #industry_sum <- reactive(get(load('data/industry_sum.Rdata'))[1])
     d<- load('data/industry_sum.Rdata')
     industry_sum <- get(d[1])
     data <- industry_sum
-    if (input$indus != "All"){
-      data <- data[data$INDUS == input$indus,]
-    }
-    if (input$members != "All"){
-      data <- data[data$Members == input$members,]
-    }
-    if (input$rankPos != "All"){
-      data <- data[data$RankPos == input$rankPos,]
+    if (input$industry_indus != "All"){
+      data <- data[data$INDUS == input$industry_indus,]
     }
     data
   }, 
@@ -33,19 +27,139 @@ shinyServer(function(input, output) {
   options = list(fnRowCallback = I('
   function(nRow, aData,iDisplayIndex, iDisplayIndexFull) {
     // Bold cells for those >= 5 in the first column
-    if (parseFloat(aData[14]) <= 3.0)
-      $("td:eq(14)", nRow).css("color", "Green");
+    if (parseFloat(aData[18]) <= 3.0)
+      $("td:eq(18)", nRow).css("color", "Green");
     else if (parseFloat(aData[14]) <= 6.0)
-      $("td:eq(14)", nRow).css("color", "Yellow");
+      $("td:eq(18)", nRow).css("color", "Yellow");
     else if (parseFloat(aData[14]) <= 9.0)
-      $("td:eq(14)", nRow).css("color", "Blue");
+      $("td:eq(18)", nRow).css("color", "Blue");
     else 
-      $("td:eq(14)", nRow).css("color", "Red");
+      $("td:eq(18)", nRow).css("color", "Red");
 
 
   }'
   ))
   
   )
+
+
+  # output for super sector
+  output$sup_sec <- renderDataTable({
+    #industry_sum <- reactive(get(load('data/industry_sum.Rdata'))[1])
+    d<- load('data/supsector_sum.Rdata')
+    industry_sum <- get(d[1])
+    data <- industry_sum
+    if (input$sup_indus != "All"){
+      data <- data[data$INDUS == input$sup_indus,]
+    }
+    if (input$sup_sup_sec != "All"){
+      data <- data[data$SUP.SEC == input$sup_sup_sec,]
+    }
+    data
+  }, 
+  
+  #options = list (bSortClasses = TRUE)  
+
+  options = list(fnRowCallback = I('
+  function(nRow, aData,iDisplayIndex, iDisplayIndexFull) {
+    // Bold cells for those >= 5 in the first column
+    if (parseFloat(aData[18]) <= 3.0)
+      $("td:eq(18)", nRow).css("color", "Green");
+    else if (parseFloat(aData[14]) <= 6.0)
+      $("td:eq(18)", nRow).css("color", "Yellow");
+    else if (parseFloat(aData[14]) <= 9.0)
+      $("td:eq(18)", nRow).css("color", "Blue");
+    else 
+      $("td:eq(18)", nRow).css("color", "Red");
+
+
+  }'
+  ))
+  
+  )
+
+
+    # output for sector
+  output$sec <- renderDataTable({
+    #industry_sum <- reactive(get(load('data/industry_sum.Rdata'))[1])
+    d<- load('data/sector_sum.Rdata')
+    industry_sum <- get(d[1])
+    data <- industry_sum
+    if (input$sec_indus != "All"){
+      data <- data[data$INDUS == input$sec_indus,]
+    }
+    if (input$sec_sup_sec != "All"){
+      data <- data[data$SUP.SEC == input$sec_sup_sec,]
+    }
+    if (input$sec_sec != "All"){
+      data <- data[data$SUP.SEC == input$sec_sec,]
+    }
+
+    data
+  }, 
+  
+  #options = list (bSortClasses = TRUE)  
+
+  options = list(fnRowCallback = I('
+  function(nRow, aData,iDisplayIndex, iDisplayIndexFull) {
+    // Bold cells for those >= 5 in the first column
+    if (parseFloat(aData[18]) <= 3.0)
+      $("td:eq(18)", nRow).css("color", "Green");
+    else if (parseFloat(aData[14]) <= 6.0)
+      $("td:eq(18)", nRow).css("color", "Yellow");
+    else if (parseFloat(aData[14]) <= 9.0)
+      $("td:eq(18)", nRow).css("color", "Blue");
+    else 
+      $("td:eq(18)", nRow).css("color", "Red");
+
+
+  }'
+  ))
+  
+  )
+
+
+
+  # output for sub sector
+  output$sub_sec <- renderDataTable({
+    #industry_sum <- reactive(get(load('data/industry_sum.Rdata'))[1])
+    d<- load('data/subsector_sum.Rdata')
+    industry_sum <- get(d[1])
+    data <- industry_sum
+    if (input$sub_indus != "All"){
+      data <- data[data$INDUS == input$sub_indus,]
+    }
+    if (input$sub_sup_sec != "All"){
+      data <- data[data$SUP.SEC == input$sub_sup_sec,]
+    }
+    if (input$sub_sec != "All"){
+      data <- data[data$SEC == input$sub_sec,]
+    }
+    if (input$sub_sub_sec != "All"){
+      data <- data[data$SUB.SEC == input$sub_sub_sec,]
+    }
+    data
+  }, 
+  
+  #options = list (bSortClasses = TRUE)  
+
+  options = list(fnRowCallback = I('
+  function(nRow, aData,iDisplayIndex, iDisplayIndexFull) {
+    // Bold cells for those >= 5 in the first column
+    if (parseFloat(aData[18]) <= 3.0)
+      $("td:eq(18)", nRow).css("color", "Green");
+    else if (parseFloat(aData[14]) <= 6.0)
+      $("td:eq(18)", nRow).css("color", "Yellow");
+    else if (parseFloat(aData[14]) <= 9.0)
+      $("td:eq(18)", nRow).css("color", "Blue");
+    else 
+      $("td:eq(18)", nRow).css("color", "Red");
+
+
+  }'
+  ))
+  
+  )
+
   
 })
--- ui.R
+++ ui.R
@@ -5,38 +5,79 @@ library(shiny)
 library(ggplot2)
 
 #load data
-d  <- load('data/industry_sum.Rdata')
+d  <- load('data/subsector_sum.Rdata')
 industry_sum <- get(d[1])
 #industry_sum <- reactive(get(load('data/industry_sum.Rdata'))[1])
 # Define the overall UI
 shinyUI(
+
   fluidPage(
-    titlePanel("Industry Sum Data"),
-          
-    # Create a new Row in the UI for selectInputs
-    fluidRow(
-      column(4, 
-          selectInput("indus", 
+    titlePanel("Data Analysis"),
+
+    # side bar of the page
+    sidebarPanel(
+      conditionalPanel(
+        'input.dataset === "INDUS"',
+        selectInput("industry_indus", 
                       "INDUS:", 
                       c("All", 
                         unique(as.character(industry_sum$INDUS))))
       ),
-      column(4, 
-          selectInput("members", 
-                      "Members:", 
-                      c("All", 
-                        unique(as.character(industry_sum$Members))))
+      conditionalPanel(
+        'input.dataset === "SUB.SEC"',
+            selectInput("sub_indus", 
+                        "INDUS:", 
+                        c("All", 
+                          unique(as.character(industry_sum$INDUS)))),
+            selectInput("sub_sup_sec", 
+                        "SUP.SEC:", 
+                        c("All", 
+                          unique(as.character(industry_sum$SUP.SEC)))),
+            selectInput("sub_sec", 
+                        "SEC", 
+                        c("All", 
+                          unique(as.character(industry_sum$SEC)))),
+            selectInput("sub_sub_sec", 
+                        "SUB.SEC", 
+                        c("All", 
+                          unique(as.character(industry_sum$SUB.SEC))))
+
       ),
-      column(4, 
-          selectInput("rankPos", 
-                      "RankPos", 
-                      c("All", 
-                        unique(as.character(industry_sum$RankPos))))
-      )        
+      conditionalPanel(
+        'input.dataset === "SUP.SEC"',
+            selectInput("sup_indus", 
+                        "INDUS:", 
+                        c("All", 
+                          unique(as.character(industry_sum$INDUS)))),
+            selectInput("sup_sup_sec", 
+                        "SUP.SEC:", 
+                        c("All", 
+                          unique(as.character(industry_sum$SUP.SEC))))
+
+      ),
+
+      conditionalPanel(
+        'input.dataset === "SEC"',
+            selectInput("sec_indus", 
+                        "INDUS:", 
+                        c("All", 
+                          unique(as.character(industry_sum$INDUS)))),
+            selectInput("sec_sup_sec", 
+                        "SUP.SEC:", 
+                        c("All", 
+                          unique(as.character(industry_sum$SUP.SEC)))),
+            selectInput("sec_sec", 
+                        "SEC", 
+                        c("All", 
+                          unique(as.character(industry_sum$SEC))))
+
+      )
+
+      
     ),
-    # Create a new row for the table.
-    fluidRow(
-      dataTableOutput(outputId="table")
-    )    
+
+    mainPanel(tabsetPanel(id='dataset',tabPanel('INDUS',dataTableOutput('industry')), tabPanel('SUP.SEC',dataTableOutput('sup_sec')), tabPanel('SEC',dataTableOutput('sec')),tabPanel('SUB.SEC',dataTableOutput('sub_sec')) ))
+    
+ 
   )  
 )
